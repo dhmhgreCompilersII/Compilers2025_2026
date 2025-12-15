00c121c57f198e5028450b1b45fe38e332ba4676
Grigoris Dimitroulakos <dhmhgre@gmail.com>
2025-12-15 13:58:22 +0200

    Add initial symbol table classes and planning diagram
    Introduced Symbol, CScopeSystem, and SymbolTable classes in the new SymbolTable.cs file to provide foundational support for symbol management and lexical scoping in the CParser project. Also added SymbolTablePlanning.vsdx Visio diagram for design planning and documentation.

---
+++ CParser/SymbolTable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CParser {
+
+
+    public class Symbol {
+        public string Name;
+        private ASTElement node;
+    }
+
+    public class CScopeSystem{
+
+        public enum CNamespaces{
+            Tags,
+            Labels,
+            Members,
+            Ordinary
+        };
+        
+
+        public void EnterScope(string name) {
+        }
+        public void ExitScope() {
+        }
+
+
+    }
+
+    public class SymbolTable{
+        private SymbolTable m_parent;
+
+        Dictionary<string,Symbol> symbols = new Dictionary<string,Symbol>();
+
+        // Lexical scoping - parent symbol table
+        public Symbol LookupSymbol(string key) {
+            if (symbols.ContainsKey(key)) {
+                return symbols[key];
+            } else if (m_parent != null) {
+                return m_parent.LookupSymbol(key);
+            } else {
+                return null;
+            }
+        }
+        public void AddSymbol(string key, Symbol symbol) {
+            if (symbols.ContainsKey(key)) {
+                throw new InvalidOperationException($"Symbol '{key}' already exists in the current scope.");
+            }
+            
+            if (symbol == null) {
+                throw new ArgumentNullException(nameof(symbol), "Cannot add null symbol to symbol table.");
+            }
+            
+            if (string.IsNullOrEmpty(symbol.Name)) {
+                throw new ArgumentException("Symbol must have a valid name.", nameof(symbol));
+            }
+            symbols.Add(key, symbol);
+        }
+
+    }
+}
