2985d5c930a5137c3443cfd8ed9859f9ab47941e
ΘΕΟΔΩΡΟΣ ΦΑΤΟΥΡΟΣ <dit22223@office365.uop.gr>
2025-12-22 12:21:45 +0200

    Refactor scope management with ScopeType enum
    Refactored CScopeSystem to introduce a ScopeType enum for clearer scope differentiation. Updated EnterScope to use ScopeType and create appropriate scope objects via a switch expression. Added new using directives and improved class formatting. ExitScope is now defined (empty), and m_Scopes is explicitly declared as a Stack<CScope>. No changes to AddSymbol or constructor logic.

--- CParser/ScopeSystem.cs
+++ CParser/ScopeSystem.cs
@@ -1,18 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Formats.Asn1;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using static CParser.CScope;
 
 namespace CParser {
-    public class CScopeSystem{
+    public class CScopeSystem
+    {
 
+        public enum ScopeType
+        {
+            File,
+            Function,
+            Block,
+            Function_Prorotype,
+            StructUnionEnum
+        };
 
-        public void EnterScope(CScope.Namespace nspace, string name) {
+        public void EnterScope(ScopeType stype, string name)
+        {
+            CScope newScope = stype switch
+            {
+                ScopeType.File => new CFileScope(),
+                ScopeType.Block => new CBlockScope(m_currentScope),
+                ScopeType.Function => new CFunctionScope(m_currentScope),
+                ScopeType.Function_Prorotype => new CFunctionScope(m_currentScope),
+                ScopeType.StructUnionEnum => new CStructUnionScope(m_currentScope),
+                _ => throw new NotImplementedException()
+            };
+            m_Scopes.Push(newScope);
+
+            if (stype == ScopeType.File)
+            {
+                m_globalScope = newScope;
+            }
+
+            m_currentScope = newScope;
         }
 
         public void ExitScope() {
+
         }
 
         public void AddSymbol(Namespace nspace, string key, Symbol symbol) {
@@ -22,6 +53,7 @@ namespace CParser {
         private CScopeSystem() {
         }
 
+        private Stack<CScope> m_Scopes;
         private CScope m_currentScope;
         private CScope m_globalScope;
         private static CScopeSystem? m_instance = null;
